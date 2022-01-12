17adaf6827833f1b2b91d1fb5cae5713e9b224c9
BLACK <yriiblack@g,ail.com>
2022-01-12 15:37:25 +0300

    3 page ready

--- lib/order.dart
+++ lib/order.dart
@@ -1,186 +1,261 @@
-import 'dart:ui';
+import 'package:flutter/material.dart'; //библиотека с дизайном от андроид
+import 'package:google_fonts/google_fonts.dart'; //библиотека со шрифтами от гугл
+import 'package:sliding_switch/sliding_switch.dart'; //библиотека для кнопки (переключателя (теста)
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+import 'package:intl/intl.dart'; // библиотека также определяет классы DateFormat , NumberFormat и BidiFormatter .
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart'; //подключение библиотеки с дизайном от андроид
-import 'package:google_fonts/google_fonts.dart'; //подключение библиотеки со шрифтами от гугл
-
-void main() { //запуск программы
-  runApp(const MyApp());
+void main() {
+  runApp(const PizzaCalculatorScreen());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PizzaCalculatorScreen extends StatefulWidget {
+  const PizzaCalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  _PizzaCalculatorScreenState createState() => _PizzaCalculatorScreenState();
+}
+
+enum Sauce { hot, sweet, cheese }
+
+class _PizzaCalculatorScreenState extends State<PizzaCalculatorScreen> {
+  bool _isSlimDough = false;
+  double _pizzaSize = 40;
+  int _cost = 100;
+  Sauce? _sauce = Sauce.hot;
+  bool _addCheese = true;
+
+  int _calcCost() {
+    _cost = _pizzaSize.round() + 100; //с округлением (round)
+
+    if (_isSlimDough == true) _cost += 30;
+    if (_addCheese == true) _cost += 50;
+
+    switch (_sauce) {
+      case Sauce.hot:
+        _cost += 10;
+        break;
+      case Sauce.sweet:
+        _cost += 20;
+        break;
+      case Sauce.cheese:
+        _cost += 40;
+        break;
+      case null:
+        _sauce = Sauce.hot;
+        break;
+    }
+    return _cost;
+  }
+
+  void _onSauceChanged(Sauce? value) {
+    setState(() {
+      _sauce = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    //общая константа для стиля границ окон ввода
-    const borderStyle = OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(36)), //радиус скругления бордюра
-        borderSide: BorderSide(
-            color: Color(0xFFbbbbbb), width: 0.01)); //цвет бордюра
 
-    return MaterialApp( //добавить остальные атрибуты
-      //не работает тема, поэтому этот костыль, глобальная переменная по оформлению шрифтов, если у шрифта нет своего определения берет эту
-      theme:ThemeData(
-        textTheme: GoogleFonts.robotoTextTheme(
-          Theme.of(context).textTheme,
-        ),
-      ),
-      home: Scaffold( //общий каркас для контейнеров и т.п.
-        backgroundColor: const Color(0xffffffff), //установка цвета общего фона
-        body: Container( //контейнер для наполнения содержимым
-          padding: const EdgeInsets.symmetric(horizontal: 0), //симметричный горизонтальный отступ
-          child: SingleChildScrollView( //прокрутка страницы содержимого внутри скобок
-            child: Column( //построение содержимого в колонку
-              children: [ //перечисление содержимого внутри колонки установлено по умолчанию константой
-
-                Row( //построение содержимого в ряд
-                  // crossAxisAlignment: CrossAxisAlignment.center, //выравнивание по второстепенной оси (в зависимости от row или column)
-                  mainAxisAlignment: MainAxisAlignment.end, //выравнивание по главной оси (в зависимости от row или column)
-                  children: [
-                    Container( //контейнер для картинки
-                        width: 232,
-                        height: 123,
-                        child: Image.asset('assets/dart-logo 3.png')),
-
-                    // const SizedBox(width: 9), //пробел
-
-                //     Text(' Регистрация', //текст
-                //         // textAlign: TextAlign.center,//центрирование
-                //         style: GoogleFonts.nunito(
-                //             textStyle: const TextStyle(
-                //               fontWeight: FontWeight.w800, //жирность шрифта 400
-                //               fontStyle: FontStyle.normal, //наклон шрифта
-                //               fontSize: 20, //размер шрифта в пикселях
-                //               letterSpacing: -0.01, //межбуквенный интервал -1%
-                //               color: Color.fromRGBO(92, 92, 92, 1), //цвет текста,
-                //               height: 0.87, //line-height высота строки
-                //             ))),
-                  ],
-                ),
-
-                const SizedBox(height: 15), //пробел
-
-                // const Divider( //подчеркивание, разделитель
-                //   thickness: 1,
-                //   height: 15,
-                //   color: Color.fromRGBO(196, 196, 196, 1.0),
-                // ),
-
-                const SizedBox(height: 7), //отступ
-
-                //текст
-                Text(
-                  'Чтобы зарегистрироваться введите\n свой номер телефона и почту', //текст к выводу
-                  textAlign: TextAlign.center,//текст к выводу
-                  style: GoogleFonts.roboto( //стиль шрифта
-                    fontStyle: FontStyle.normal, //наклон шрифта
-                    fontWeight: FontWeight.w400, //жирность шрифта 400
-                    fontSize: 19, //размер шрифта в пикселях
-                    letterSpacing: -0.015, //межбуквенный интервал
-                    color: const Color.fromRGBO(196, 196, 196, 1.0), //цвет текста
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 62,
                   ),
-                ),
-                const SizedBox(height: 14), //отступ
-                //заголовок
-                Text('Телефон',
-                    style: GoogleFonts.roboto(
-                        textStyle: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal,
-                          letterSpacing: -0.015,
-                          color: Color.fromRGBO(0, 0, 0, 0.6),
-                        ))),
+                SizedBox(
+                  height: 100,
+                  child: Image.asset('assets/dart-logo 3.png'),
+                 ),
 
-                const SizedBox(height: 6), //отступ
-
-                //поле ввода телефона
-                const TextField(
-                  keyboardType: TextInputType.phone, //способ вывода клавиатуры
-                  decoration: InputDecoration( //оформление декорации-фон на фоне
-                    filled: true,
-                    fillColor: Color(0xFFeceff1), //цвет декорации
-                    enabledBorder: borderStyle, //отображение границ окна ввода до нажатия, см. общую константу
-                    focusedBorder: borderStyle, //отображение границ окна ввода после нажатия-фокусировки см. общую константу
-                    // labelText: '+7', //отображаемый текст до нажатия
-                    hintText: '+7', //отображаемый текст при активации окна
+                Text('Калькулятор пиццы',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.inter(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 30,
+                    color: const Color.fromRGBO(0, 0, 0, 1),
                   ),
                 ),
 
-                const SizedBox(height: 14,), //отступ
+                SizedBox(height: 9),
 
-                //заголовок
-                Text('Почта',
-                    style: GoogleFonts.roboto(
-                        textStyle: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal,
-                          letterSpacing: -0.015,
-                          color: Color.fromRGBO(0, 0, 0, 0.6),
-                        ))),
-
-                const SizedBox(height: 6,), //отступ
-
-                //поле ввода почты
-                const TextField(
-                  obscureText: true, //скрыть пароль при введении
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    enabledBorder: borderStyle, //отображение границ окна ввода до нажатия, см. общую константу
-                    focusedBorder: borderStyle, //отображение границ окна ввода после нажатия-фокусировки см. общую константу
+                Text('Выберите параметры:',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.inter(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    color: const Color.fromRGBO(0, 0, 0, 1), //цвет текста
                   ),
                 ),
 
-                const SizedBox(height: 24),
-                Text(
-                  'Вам придет четырехзначный код,\n который будет вашим паролем', //текст к выводу
-                  textAlign: TextAlign.center,//текст к выводу
-                  style: GoogleFonts.roboto( //стиль шрифта
-                    fontStyle: FontStyle.normal, //наклон шрифта
-                    fontWeight: FontWeight.normal, //жирность шрифта 400
-                    fontSize: 16, //размер шрифта в пикселях
-                    letterSpacing: -0.015, //межбуквенный интервал
-                    color: const Color.fromRGBO(0, 0, 0, 0.6), //цвет текста
+                SizedBox(height: 12),
+
+                SizedBox(
+                  width: 300,
+                  child: SlidingSwitch(
+                    value: false,
+                    width: 300,
+                    onChanged: (bool value) {
+                    _isSlimDough = value;
+                    setState(() {
+                    _calcCost();
+                    });
+                    },
+
+                    height: 30,
+                    animationDuration : const Duration(milliseconds: 100),
+                    onTap: () {},
+                    onDoubleTap: () {},
+                    onSwipe: () {},
+                    textOff: "Обычное тесто",
+                    textOn: "Тонкое тесто",
+                    colorOn: const Color(0xffFFFFFF),
+                    colorOff: const Color(0xf0FFFFFF),
+                    background: const Color(0xffECEFF1),
+                    buttonColor: const Color(0xff0079D0),
+                    inactiveColor: const Color(0xff636f7b),
+                   ),
                   ),
+
+                const SizedBox(height: 10),
+
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(top: 20, left: 20),
+                  child: const Text('Размер',
+                    style: TextStyle(fontSize: 18, color: Color(0xFF000000))),
                 ),
 
-                //отступ
-                const SizedBox(height: 14), //отступ
-                //текст
-                Text(
-                  'Изменить пароль можно\n будет в личном кабинете после\n регистрации', //текст к выводу
-                  textAlign: TextAlign.center,//текст к выводу
-                  style: GoogleFonts.roboto( //стиль шрифта
-                    fontStyle: FontStyle.normal, //наклон шрифта
-                    fontWeight: FontWeight.normal, //жирность шрифта 400
-                    fontSize: 16, //размер шрифта в пикселях
-                    letterSpacing: -0.015, //межбуквенный интервал
-                    color: const Color.fromRGBO(0, 0, 0, 0.6), //цвет текста
-                  ),
+                SizedBox(
+                  width: 300,
+                  child: SfSlider(
+                    min: 20,
+                    max: 60,
+                    value: _pizzaSize,
+                    interval: 20,
+                    showTicks: true,
+                    showLabels: true,
+                    numberFormat: NumberFormat("## cm"),
+                    enableTooltip: false,
+                    minorTicksPerInterval: 0,
+                    stepSize: 20,
+                    onChanged: (dynamic value){
+                    setState(() {
+                    _pizzaSize = value;
+                    _calcCost();
+                  });
+                 },
+                )),
+
+                const SizedBox(height: 10),
+
+                Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(top: 10, left: 20),
+                child: const Text('Соус',
+                  style: TextStyle(fontSize: 18, color: Color(0xFF000000))),
                 ),
 
-                const SizedBox(height: 32), //отступ
+                RadioListTile<Sauce>(
+                  title: const Text ('Острый'),
+                  value: Sauce.hot,
+                  groupValue: _sauce,
+                  onChanged: _onSauceChanged,
+                  visualDensity: const VisualDensity(horizontal: 0, vertical: -4.0),
+                ),
+                RadioListTile<Sauce>(
+                  title: const Text ('Кисло-сладкий'),
+                  value: Sauce.sweet,
+                  groupValue: _sauce,
+                  onChanged: _onSauceChanged,
+                  visualDensity: const VisualDensity(horizontal: 0, vertical: -4.0),
+                ),
+                RadioListTile<Sauce>(
+                  title: const Text ('Сырный'),
+                  value: Sauce.cheese,
+                  groupValue: _sauce,
+                  onChanged: _onSauceChanged,
+                  visualDensity: const VisualDensity(horizontal: 0, vertical: -4.0),
+                ),
 
-                //кнопка, добавить остальные атрибуты шрифта
-                SizedBox(width: 154, height: 42, child:
-                ElevatedButton(onPressed: () {},
-                  child: const Text('Отправить код'),
-                  style: ElevatedButton.styleFrom( //формат кнопка
-                    primary: const Color(0xFF0079D0), //цвет
-                    shape: RoundedRectangleBorder( //форма Rectangle-прямоугольник
-                      borderRadius: BorderRadius.circular(22.0), //радиус скргленный
+                SizedBox(
+                  width:300,
+                  child: Card(
+                    elevation: 0,
+                    color: Color(0xFFF0F0F0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 20,
+                          ),
+                          child: SizedBox(
+                          width:38,
+                          height: 56,
+                            child: Image.asset('assets/dart-logo 4.png'),
+                           ),
+                          ),
+
+                        Text('Дополнительный сыр:',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.inter(
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                            //letterSpacing: -0.015,
+                            color: const Color.fromRGBO(0, 0, 0, 1)
+                          )
+                        ),
+
+                        Switch(
+                          value: _addCheese,
+                          onChanged: (bool value) {
+                           setState(() {
+                             _addCheese = value;
+                             _calcCost();
+                            });
+                           }),
+                       ],
                     ),
                   ),
                 ),
+
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(top: 10, left: 20),
+                  child: Text('Стоимость',
+                    style: TextStyle(fontSize: 18, color: Color(0xFF000000))),
+                  ),
+
+                SizedBox(
+                  width: 300,
+                  height: 50,
+
+                  child: Card(
+                    elevation: 0,
+                    color: const Color(0xFFF0F0F0),
+                    shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(10)),
+                    child:Text('${_calcCost()} руб.',
+                      style: const TextStyle(fontSize: 32, color: Color(0xFF000000)),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
 
-                const SizedBox(height: 84,), //отступ
-              ],  //Children
-            ),
-          ),
+              const SizedBox(height: 40),
+          ],
         ),
       ),
+    )
     );
   }
 }
